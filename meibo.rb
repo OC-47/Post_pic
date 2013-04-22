5d373abd86afe4ccebe901c65deea289b01001a2
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-04-22 23:16:42 +0900

    print exel

--- meibo.rb
+++ meibo.rb
@@ -10,15 +10,8 @@ class Student < ActiveRecord::Base
 end
 
 get '/' do
-  "Hello World"
+	@students = Student.all
+	erb :index
 end
 
 
-# idが「1234567890」の学生だけ抽出しオブジェクトに格納します．
-#student = Student.find('1234567890')
-#puts student.id
-#puts student.name
-students = Student.all
-students.each do |stu|
-  puts stu.id + "\t" + stu.name + "\t" + stu.email
-end
